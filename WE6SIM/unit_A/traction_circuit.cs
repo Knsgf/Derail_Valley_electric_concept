e44c6444e98010f35f982d7b49e061640cfd99fc
Knsgf <kf177045@hotmail.com>
2026-06-06 11:35:52 +0300

    Tweaked resistances

--- WE6SIM/unit_A/traction_circuit.cs
+++ WE6SIM/unit_A/traction_circuit.cs
@@ -129,12 +129,12 @@ internal partial class unit_A_sim
     {
         for (int resistor_branch = 1; resistor_branch <= 3; ++resistor_branch)
         {
-            _base_element_resistances[$"SR{resistor_branch}.1"] = 0.65f;
-            _base_element_resistances[$"SR{resistor_branch}.2"] = 2.2f;
-            _base_element_resistances[$"SR{resistor_branch}.3"] = 2.565f;
-            _base_element_resistances[$"SR{resistor_branch}.4"] = 0.063f;
-            _base_element_resistances[$"SR{resistor_branch}.5"] = 0.129f;
-            _base_element_resistances[$"SR{resistor_branch}.6"] = 0.183f;
+            _base_element_resistances[$"SR{resistor_branch}.1"] = 0.67f;
+            _base_element_resistances[$"SR{resistor_branch}.2"] = 1.35f;
+            _base_element_resistances[$"SR{resistor_branch}.3"] = 1.93f;
+            _base_element_resistances[$"SR{resistor_branch}.4"] = 0.06f;
+            _base_element_resistances[$"SR{resistor_branch}.5"] = 0.115f;
+            _base_element_resistances[$"SR{resistor_branch}.6"] = 0.165f;
         }
         for (int motor_branch = 1; motor_branch <= motors; ++motor_branch)
         {
