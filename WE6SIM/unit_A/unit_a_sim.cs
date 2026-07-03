ca10e6683a8d321ff0e56b5dae0ce1e8b5a0ee38
Knsgf <kf177045@hotmail.com>
2026-07-03 19:02:03 +0300

    Electrical powertrain damage now raises resistance
    Electrical powertrain damage above 50 % raises input resistance by 1 Ohm per 10 %.

--- WE6SIM/unit_A/unit_a_sim.cs
+++ WE6SIM/unit_A/unit_a_sim.cs
@@ -35,7 +35,7 @@ internal partial class unit_A_sim: electric_device
     private readonly Fuse _appliances, _control_air, _main_breaker_closed, _compressor_on;
     private readonly Port _torque_A, _wheel_RPM, _traction_motor_load, _traction_motor_RPM, _traction_motor_EMF, _jog_volts;
     private readonly Port _contactor_on_sound, _contactor_off_sound;
-    private readonly Port _total_load, _relative_voltage, _compressor_power, _traction_motor_heat_B;
+    private readonly Port _total_load, _relative_voltage, _compressor_power, _traction_motor_heat_B, _integrity;
     private readonly Port _reverser_handle, _selector_handle;
 
     private readonly Port _control_AB1, _control_BA1, _control_BA2, _torque_B, _wheel_RPM_B, _traction_motor_load_B;
@@ -64,10 +64,13 @@ internal partial class unit_A_sim: electric_device
 
     private readonly contactors _contactors;
 
-    private bool _fast_notching_enabled = false, _jogging_mode_on = false, _jog = false, _cab_active = false;
-    private int  _throttle = -1, _secondary_camshaft_notch = 1, _selector = -1, _field_position = -1;
+    private readonly float _motor_voltmeter_resistance;
+
+    private bool  _fast_notching_enabled = false, _jogging_mode_on = false, _jog = false, _cab_active = false;
+    private int   _throttle = -1, _secondary_camshaft_notch = 1, _selector = -1, _field_position = -1;
     private Task? _single_notch_movement;
     private float _reverser_position = 0.5f, _fast_notching_current_limit = 250.0f;
+    private float _last_integrity = -1.0f, _resistance_update_time = 0.0f;
 
     public const int camshaft_notches = 7, roll_over_to_1 = camshaft_notches + 1, roll_over_to_full = camshaft_notches + 2;
 
@@ -86,6 +89,7 @@ internal partial class unit_A_sim: electric_device
 
         _torque_A            = grab_port(ports, "traction.TORQUE_IN"                        );
         _wheel_RPM           = grab_port(ports, "traction.WHEEL_RPM_EXT_IN"                 );
+        _integrity           = grab_port(ports, "[CustomSimulation].MOTOR_INTEGRITY"        );
         _traction_motor_load = grab_port(ports, "[CustomSimulation].MOTOR_LOAD"             );
         _traction_motor_RPM  = grab_port(ports, "[CustomSimulation].MOTOR_RPM"              );
         _traction_motor_EMF  = grab_port(ports, "[CustomSimulation].MOTOR_EMF"              );
@@ -99,6 +103,7 @@ internal partial class unit_A_sim: electric_device
         UnityEngine.Random.InitState(random_seed);
         foreach (KeyValuePair<string, float> element in _base_element_resistances)
             _element_resistances[element.Key] = element.Value * UnityEngine.Random.Range(1.0f - variation, 1.0f + variation);
+        _motor_voltmeter_resistance = _element_resistances["VM34"];
         float[] motor_EMF_variations = new float[motors], motor_torque_variations = new float[6];
         for (int motor_index = 0; motor_index < motors; ++motor_index)
         {
@@ -504,13 +509,29 @@ internal partial class unit_A_sim: electric_device
             total_heat   += motor.heat_emission;
         }
     }
-    
+
+    private void set_powertrain_damage_resistance()
+    {
+        _resistance_update_time -= Time.deltaTime;
+        float current_integrity  = _integrity.Value;
+        if (_resistance_update_time <= 0.0f || current_integrity > _last_integrity)
+        {
+            _named_branches["EPS"].closed_resistance = 0.001f + ((current_integrity >= 0.5f) ? 0.0f : ((0.5f - current_integrity) * 10.0f));
+            //Main.log($"{_named_branches["EPS"].closed_resistance} ohm");
+            _resistance_update_time = 10.0f;
+            _last_integrity         = current_integrity;
+        }
+    }
+
     private void simulate()
     {
         check_if_disposed();
         
-        blower_controller blowers = _blowers;
-        bool                  jog = _jogging_mode_on && !is_powered;
+        blower_controller               blowers        = _blowers;
+        bool                            jog            = _jogging_mode_on && !is_powered;
+		Dictionary<string, branch_user> named_branches = _named_branches;
+        branch_user                     supply         = named_branches["EPS"];
+        Dictionary<string,       float> currents       = _currents;
         if (jog)
         {
             if (!_jog)
@@ -518,7 +539,7 @@ internal partial class unit_A_sim: electric_device
                 _contactors.toggle_jogging(turn_on: true);
                 _jog = true;
             }
-            _total_load.Value = _currents["BAT"];
+            _total_load.Value = currents["BAT"];
             _jog_volts.Value  = battery_panel.battery_EMF - _total_load.Value * battery_panel.battery_internal_resistance;
             _pantograph.simulate(0.0f);
         }
@@ -529,9 +550,9 @@ internal partial class unit_A_sim: electric_device
                 _contactors.toggle_jogging(turn_on: false);
                 _jog = false;
             }
-            _total_load.Value = _currents["EPS"] + blowers.current_draw;
-            if (_named_branches["EPS"].EMF > 1.0f)
-                _total_load.Value += _compressor_power.Value / _named_branches["EPS"].EMF;
+            _total_load.Value = currents["EPS"] + blowers.current_draw;
+            if (supply.EMF > 1.0f)
+                _total_load.Value += _compressor_power.Value / supply.EMF;
             _jog_volts.Value  = 0.0f;
             _pantograph.simulate(_total_load.Value);
         }
@@ -541,44 +562,44 @@ internal partial class unit_A_sim: electric_device
         toggle_port_signal(_control_AB1, (int) AB1_signals.contactor_on , false);
         toggle_port_signal(_control_AB1, (int) AB1_signals.contactor_off, false);
 
-        if (!jog && !_main_breaker_closed.State && _roof_bus.voltage < 10.0f && _named_branches["EPS"].EMF < 10.0f && _wheel_RPM.Value < 20.0f 
+        if (!jog && !_main_breaker_closed.State && _roof_bus.voltage < 10.0f && supply.EMF < 10.0f && _wheel_RPM.Value < 20.0f 
             && blowers.motor_current < 10.0f && blowers.relative_speed < 0.01f && _regenerative_field.relative_speed < 0.01f)
         {
-            _torque_A.Value = _torque_B.Value = 0.0f;
+            _torque_A.Value = _torque_B.Value = _resistance_update_time = 0.0f;
             //_traction_motor_temperature.simulate(0.0f);
         }
         else
         {
-            Dictionary<string, float> currents = _currents;
             lock (currents)
             {
                 //circuit_telemetry.log_sorted_currents(_circuit, -1.0f, -1.0f);
                 //circuit_telemetry.log_sorted_voltages(_circuit);
-                foreach (KeyValuePair<string, branch_user> branch in _named_branches)
+                foreach (KeyValuePair<string, branch_user> branch in named_branches)
                     currents[branch.Key] = currents[branch.Key] * 0.95f + branch.Value.current * 0.05f;
             }
-            float motors_volts = Mathf.Abs(currents["VM34"] * _element_resistances["VM34"]);
+            float motors_volts = Mathf.Abs(currents["VM34"] * _motor_voltmeter_resistance);
             _resistor_grid.simulate(currents);
             resistor_heat.simulate_overheat_damage(_resistor_grid);
 
             bool  rheostatic_brake_on = _selector is (int) selector_modes.rheostatic_brake;
             float voltage = rheostatic_brake_on ? 0.0f : _roof_bus.voltage;
-            _named_branches["EPS"].EMF = _named_branches["EPS"].EMF * 0.9f + voltage * 0.1f;
-            bool regenerative_on = _selector is (int) selector_modes.series_regenerative or (int) selector_modes.parallel_regenerative;
-            if (regenerative_on || _regenerative_field.relative_speed >= 0.01f)
-                _regenerative_field.simulate(regenerative_on, _field_position, _roof_bus.voltage, motors_volts);
+            supply.EMF = supply.EMF * 0.9f + voltage * 0.1f;
         
+            set_powertrain_damage_resistance();
             traction_motor[] traction_motors = _traction_motors;
             for (int motor_index = motors - 1; motor_index >= 0; --motor_index)
-                traction_motors[motor_index].simulate(rheostatic_brake_on && _throttle >= 1, currents, _named_branches);
+                traction_motors[motor_index].simulate(rheostatic_brake_on && _throttle >= 1, currents, named_branches);
             _circuit.simulate();
 
-            float voltmeter_reading = rheostatic_brake_on ? blowers.fan_voltage : (_named_branches["EPS"].EMF - currents["EPS"] * _element_resistances["EPS"]);
+            float voltmeter_reading = rheostatic_brake_on ? blowers.fan_voltage : (supply.EMF - currents["EPS"] / supply.conductance);
             set_supply_volts(voltmeter_reading);
             _relative_voltage.Value = voltmeter_reading / 1500.0f;
             set_motors_volts(motors_volts);
+            bool regenerative_on = _selector is (int) selector_modes.series_regenerative or (int) selector_modes.parallel_regenerative;
+            if (regenerative_on || _regenerative_field.relative_speed >= 0.01f)
+                _regenerative_field.simulate(regenerative_on, _field_position, voltmeter_reading, motors_volts);
             
-            _compressor_on.ChangeState(voltage >= 1000.0f && _main_breaker_closed.State);
+            _compressor_on.ChangeState(voltmeter_reading >= 1000.0f && _main_breaker_closed.State);
             float average_RPM = 0.0f, average_load_A = 0.0f, average_load_B = 0.0f, maximum_load = 0.0f; 
             float average_field_A = 0.0f, average_field_B = 0.0f, average_EMF = 0.0f;
             float total_torque_A, total_heat_emission_A, total_torque_B, total_heat_emission_B;
@@ -588,7 +609,7 @@ internal partial class unit_A_sim: electric_device
             calculate_combined_unit_motor_performance(is_unit_A: false, traction_motors, ref average_RPM, 
                 ref average_load_B, ref maximum_load, ref average_field_B, ref average_EMF,
                 out total_torque_B, out total_heat_emission_B);
-            _main_breaker.trip_if_operating_parameters_exceeded(voltage, motors_volts, maximum_load, _total_load.Value);
+            _main_breaker.trip_if_operating_parameters_exceeded(voltmeter_reading, motors_volts, maximum_load, _total_load.Value);
             average_RPM     /= motors;
             average_EMF     /= motors;
             average_load_A  /= (motors >> 1);
@@ -630,7 +651,7 @@ internal partial class unit_A_sim: electric_device
             blowers.active                = rheostatic_brake_on || /*_primary_controller.current_notch > 1*/ _throttle >= 1;
             blowers.rheostatic_braking_on = rheostatic_brake_on;
             blowers.motor_current         = maximum_load;
-            blowers.line_voltage          = rheostatic_brake_on ? motors_volts : voltage;
+            blowers.line_voltage          = rheostatic_brake_on ? motors_volts : voltmeter_reading;
             blowers.simulate();
             _traction_motor_temperature.simulate(total_heat_emission_A, average_RPM);
 
