7edb936266c1389b217dc617234c9ce27ddb139c
Knsgf <kf177045@hotmail.com>
2026-07-08 15:43:19 +0300

    Unity project and Blender source file

---
+++ unity_project/Assets/Scenes/print_all_object_with_component.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using CCL.Types.Proxies.Controls;
+
+using UnityEngine;
+
+public class print_all_object_with_component: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var objects = FindObjectsOfType<LeverProxy>();
+        foreach (var current_object in objects)
+            print(current_object.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
