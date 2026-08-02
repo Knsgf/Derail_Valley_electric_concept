5146a4a0a34c68747ed041ccc9d8c2453373c752
Knsgf <kf177045@hotmail.com>
2026-08-02 16:37:53 +0300

    Refactor of selector conformance

--- WE6SIM/devices/selector_interlock.cs
+++ WE6SIM/devices/selector_interlock.cs
@@ -14,16 +14,20 @@ internal class selector_interlock(Action<float> unit_selector_handler, float han
 {
     private readonly Action<float> unit_selector_handler = unit_selector_handler;
 
-    private float _current_selector = handle_initial_position;
+    private float _current_selector = raw_selector_to_notch(handle_initial_position);
 
+    private static float raw_selector_to_notch(float raw_selector)
+    {
+        return Mathf.Clamp(Mathf.RoundToInt(raw_selector * selector_last_notch), 0.0f, selector_last_notch) / selector_last_notch;
+    }
+    
     public void interlocked_handler(float raw_selector, float raw_throttle, int primary_notch, int secondary_notch,
         Port? transition_lamp)
     {
         if (   raw_throttle                                < 0.5f / throttle_notches 
             || Mathf.Abs(raw_selector - _current_selector) < 0.5f / selector_notches)
         {
-            _current_selector = Mathf.Clamp(Mathf.RoundToInt(raw_selector * selector_last_notch), 0.0f, selector_last_notch) 
-                              / selector_last_notch;
+            _current_selector = raw_selector_to_notch(raw_selector);
             if (transition_lamp != null && transition_lamp.Value >= 0.7f)
                 transition_lamp.Value = 0.0f;
             unit_selector_handler(raw_selector);
@@ -36,8 +40,7 @@ internal class selector_interlock(Action<float> unit_selector_handler, float han
                 && new_selector is (int) selector_modes.series_power or (int) selector_modes.parallel_power 
                 && (primary_notch <= 6 || secondary_notch <= 6))
             {
-                _current_selector = Mathf.Clamp(Mathf.RoundToInt(raw_selector * selector_last_notch), 0.0f, selector_last_notch) 
-                                  / selector_last_notch;
+                _current_selector = raw_selector_to_notch(raw_selector);
                 unit_selector_handler(raw_selector);
             }
             else
